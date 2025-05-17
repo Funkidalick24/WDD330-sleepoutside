dec041fe7c109a0b6572a9b963984ffed4c3be6f
Kummirai <ajaxmilton@hotmail.com>
2025-05-18 01:37:16 +0200

    Fix merge conflicts in utils.mjs by removing leftover conflict markers

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,10 +21,6 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 448a718 (Merge pull request #9 from Funkidalick24/mk--individual)
 // get the product id from the query string
 export function getParam(param) {
   const queryString = window.location.search;
@@ -40,8 +36,4 @@ export function renderListWithTemplate(template, parentElement, list, position =
     parentElement.innerHTML = "";
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> parent of 448a718 (Merge pull request #9 from Funkidalick24/mk--individual)
