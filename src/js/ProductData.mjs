8778f5863eeb2414487e290786c5c998d4dc66d3
Hollypie <hbriggs@byupathway.edu>
2025-05-21 18:39:31 -0600

    really tried to do week3 team project but failed utterly

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,8 +9,7 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `/json/${this.category}.json`;
-    // this.path = `../json/${this.category}.json`;  old code in case new code breaks things
+    this.path = `../public/json/${this.category}.json`;
   }
   getData() {
     return fetch(this.path)
